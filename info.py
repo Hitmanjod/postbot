e7449d31650a87b6cf7dfc27744a830e47e9b02b
SujanCh564 <132272276+SujanCh564@users.noreply.github.com>
2023-08-16 16:32:39 +0545

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,6 @@ API_ID       = 25502576
 API_HASH     = "f0f35dbb5b0081cdc8d3c9d5383c4628"
 BOT_TOKEN    = "6160188891:AAFIfbcCltVOEz40mK9lsmvpYnG7NATRHqA"
 SESSION      = "BQC6kfsAH_hm3FfOvfhOKFCcrbcwuhPqk0poh4t4Zb9xSJ5vlIUtOfZovHM7KJEmAuKTv5UdDTyinm_G-mUhrSSyXdnUAZdqyOhv6Qp6GW-PYyr4zPy7ykV_53yVwYaDqwQbx7Q2DYc70x5BZLgQPMjWXhxHEzhvEIZUnNqw0Bh1mZtvGgVOBszAYeCAY63-tNr36LQtnhuQtVSLz7aS2DEKOmXGFsqSzkzZie0KwhmfHubTy_tl2e3rKTVhXagBydZGqDUnddW7CSYApFxMmD5qJUQ4qzR5oqGCufc9l53S_T9LoAI8Msl8jxiH4fvGTeVw1MexJsZZ_mmIAZESdVBbnAZa7wAAAAFqOL-BAA"
-DATABASE_URI = "mongodb+srv://sujanch9993:sujanch9993@cluster0.yhmdrl9.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://sujanch846:sujanch846@cluster0.sqvan6z.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001948924720
 ADMIN        = 5123039648
